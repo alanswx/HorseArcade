f9afa6328348131d7982bfe44b9eb36e54eb8384
Thomas Upin <tmutmu@private.com>
2018-10-07 17:45:06 -0700

    added null file

--- dispense.py
+++ dispense.py
@@ -27,18 +27,16 @@ import Adafruit_PCA9685
 pwm = Adafruit_PCA9685.PCA9685()
 def dispense_init():
     pwm.set_pwm_freq(60)
-    servo_min = config.servo_min
-    servo_max = config.servo_max
-    pwm.set_pwm(0, 0, servo_min)
-    pwm.set_pwm(1, 0, servo_min)
-    pwm.set_pwm(2, 0, servo_min)
-    pwm.set_pwm(3, 0, servo_min)
+    pwm.set_pwm(0, 0, config.servo_min)
+    pwm.set_pwm(1, 0, config.servo_min)
+    pwm.set_pwm(2, 0, config.servo_min)
+    pwm.set_pwm(3, 0, config.servo_min)
     print("Initializing servo")
 def dispense_back(channel):
     # Move servo on channel O between extremes.
-    pwm.set_pwm(channel, 0, servo_min)
+    pwm.set_pwm(channel, 0, config.servo_min)
     print("Dispenser " + str(channel+1)+": Pulling Back")
 def dispense_forward(channel):
-    pwm.set_pwm(channel, 0, servo_max)
+    pwm.set_pwm(channel, 0, config.servo_max)
     time.sleep(1)
     print("Dispenser " + str(channel+1)+": Dispensing Candy")
