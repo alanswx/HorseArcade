7f072f7e6d9435d44b9811e94a17058646b4cbc2
Thomas Upin <tmutmu@private.com>
2018-10-07 17:32:14 -0700

    added null file

---
+++ dispensenull.py
@@ -0,0 +1,6 @@
+def dispense_init():
+    print("Initializing servo")
+def dispense_back(channel = None):
+    print("Dispenser " + str(channel+1)+": Pulling Back")
+def dispense_forward(channel = None):
+    print("Dispenser " + str(channel+1)+": Dispensing Candy")
