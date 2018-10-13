07344bb036f15d01207f6bc50fcc7dc1d843975d
Thomas Upin <tmutmu@private.com>
2018-10-13 16:33:32 -0700

    bug fixes

--- horse.py
+++ horse.py
@@ -438,6 +438,7 @@ class Finish(States):
             self.markDone()
           elif event.key == pygame.K_ESCAPE: self.quit = True
     def markDone(self):
+        print("markDone")
       #dt = time.time() - self.showTimer
     def update(self, screen, dt):
         self.draw(screen)
